be92b6c0a417557f7f1f27b7dadc2ab110d8a7dc
jackie247s <jackie247s@gmail.com>
2017-06-11 15:25:33 +0500

    Added logo, converted to .php and separated header into separate php include file

---
+++ includes/header.php
@@ -0,0 +1,70 @@
+<header>
+  <div class="row header-top">
+    <div class="col-md-4 logo">
+      <a href="index.html"><img id="logo" src="images/logo.jpg" alt="Logo"></a>
+    </div>
+    <div class="col-md-4">
+
+    </div>
+    <div class="col-md-4 top-right">
+      <a href="#">Home</a>
+      <a href="#">Contact Us</a>
+      <a href="#">Downloads</a>
+    </div>
+  </div>
+
+  <div class="header-bottom">
+    <nav class="navbar navbar-inverse">
+     <div class="container-fluid">
+
+       <ul class="nav navbar-nav">
+
+         <li class="active dropdown">
+           <a class="dropdown-toggle" data-toggle="dropdown" href="#">ABOUT US
+             <span class="caret"></span></a>
+           <ul class="dropdown-menu">
+             <li><a href="#">Chairman &amp; CEO's Message</a></li>
+             <li><a href="#">Mission &amp; Vision</a></li>
+             <li><a href="#">Organization Chart</a></li>
+             <li><a href="#">Company Profile</a></li>
+           </ul>
+         </li>
+
+         <li class="dropdown">
+           <a class="dropdown-toggle" data-toggle="dropdown" href="#">CREDENTIAL
+           <span class="caret"></span></a>
+           <ul class="dropdown-menu">
+             <li><a href="#">Page 1-1</a></li>
+             <li><a href="#">Page 1-2</a></li>
+             <li><a href="#">Page 1-3</a></li>
+           </ul>
+         </li>
+
+         <li class="dropdown">
+           <a class="dropdown-toggle" data-toggle="dropdown" href="#">DIVISIONS
+           <span class="caret"></span></a>
+           <ul class="dropdown-menu">
+             <li><a href="#">Page 1-1</a></li>
+             <li><a href="#">Page 1-2</a></li>
+             <li><a href="#">Page 1-3</a></li>
+           </ul>
+         </li>
+
+         <li class="dropdown">
+           <a class="dropdown-toggle" data-toggle="dropdown" href="#">PROJECTS
+           <span class="caret"></span></a>
+           <ul class="dropdown-menu">
+             <li><a href="#">Projects Ongoing</a></li>
+             <li><a href="#">Projects Completed</a></li>
+           </ul>
+         </li>
+
+         <li><a href="#">NEWS &amp; EVENTS</a></li>
+         <li><a href="#">CAREER</a></li>
+         <li><a href="#">ENQUIRY</a></li>
+       </ul>
+     </div>
+    </nav>
+  </div>
+
+</header>
--- index.php
+++ index.php
@@ -11,76 +11,9 @@
   <body>
 
     <div class="page">
-      <header>
-        <div class="row header-top">
-          <div class="col-md-4 logo">
-            <a href="index.html"><h2>FutureLeap</h2></a>
-          </div>
-          <div class="col-md-4">
-
-          </div>
-          <div class="col-md-4 top-right">
-            <a href="#">Home</a>
-            <a href="#">Contact Us</a>
-            <a href="#">Downloads</a>
-          </div>
-        </div>
-
-        <div class="header-bottom">
-          <nav class="navbar navbar-inverse">
-           <div class="container-fluid">
-
-             <ul class="nav navbar-nav">
-
-               <li class="active dropdown">
-                 <a class="dropdown-toggle" data-toggle="dropdown" href="#">ABOUT US
-                   <span class="caret"></span></a>
-                 <ul class="dropdown-menu">
-                   <li><a href="#">Chairman &amp; CEO's Message</a></li>
-                   <li><a href="#">Mission &amp; Vision</a></li>
-                   <li><a href="#">Organization Chart</a></li>
-                   <li><a href="#">Company Profile</a></li>
-                 </ul>
-               </li>
-
-               <li class="dropdown">
-                 <a class="dropdown-toggle" data-toggle="dropdown" href="#">CREDENTIAL
-                 <span class="caret"></span></a>
-                 <ul class="dropdown-menu">
-                   <li><a href="#">Page 1-1</a></li>
-                   <li><a href="#">Page 1-2</a></li>
-                   <li><a href="#">Page 1-3</a></li>
-                 </ul>
-               </li>
-
-               <li class="dropdown">
-                 <a class="dropdown-toggle" data-toggle="dropdown" href="#">DIVISIONS
-                 <span class="caret"></span></a>
-                 <ul class="dropdown-menu">
-                   <li><a href="#">Page 1-1</a></li>
-                   <li><a href="#">Page 1-2</a></li>
-                   <li><a href="#">Page 1-3</a></li>
-                 </ul>
-               </li>
-
-               <li class="dropdown">
-                 <a class="dropdown-toggle" data-toggle="dropdown" href="#">PROJECTS
-                 <span class="caret"></span></a>
-                 <ul class="dropdown-menu">
-                   <li><a href="#">Projects Ongoing</a></li>
-                   <li><a href="#">Projects Completed</a></li>
-                 </ul>
-               </li>
-
-               <li><a href="#">NEWS &amp; EVENTS</a></li>
-               <li><a href="#">CAREER</a></li>
-               <li><a href="#">ENQUIRY</a></li>
-             </ul>
-           </div>
-          </nav>
-        </div>
-
-      </header>
+      <?php
+        require_once 'includes/header.php';
+       ?>
 
       <div class="slider">
         <ul class="bxslider">
@@ -138,7 +71,7 @@
         <div class="project-header">
           <h2>LATEST PROJECTS</h2>
         </div>
-        
+
       </div>
 
       <footer>
