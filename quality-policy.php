e6ae8865e2918a0aaae80a912fcaa5f7cea9f73f
jackie247s <jackie247s@gmail.com>
2017-06-11 16:03:52 +0500

    Put content in quality policy page in container-fluid

--- quality-policy.php
+++ quality-policy.php
@@ -14,17 +14,25 @@
         require_once 'includes/header.php';
        ?>
 
-       <div class="quality-policy col-md-8">
-         <h2>Quality Policy</h2>
-         <p>Our quality policy has been defined with the aim of enhancing our services to our customers in terms of better understanding of customer requirements, designing optimal solutions to meet those requirements and building systems and processes to ensure the highest standards of quality at all costs.We are committed to:</p>
-         <ul>
-           <li>Developing and improving Quality Management System periodically</li>
-           <li>Monitoring process towards building quality throughout our systems &amp; services.</li>
-           <li>Systematically evaluate and monitor the performance of our team</li>
-           <li>Provide timely delivery of services to our clients</li>
-           <li>Identify bottlenecks and subsequently eliminating them</li>
-           <li>Provide periodical training to our whole team so they understand the gravity of adhering to our Quality Management Protocol</li>
-         </ul>
+       <div class="quality-policy col-md-8 col-lg-8">
+
+         <div class="container-fluid">
+           <h2>Quality Policy</h2>
+           <p>Our quality policy has been defined with the aim of enhancing our services to our customers in terms of better understanding of customer requirements, designing optimal solutions to meet those requirements and building systems and processes to ensure the highest standards of quality at all costs.We are committed to:</p>
+           <ul>
+             <li>Developing and improving Quality Management System periodically</li>
+             <li>Monitoring process towards building quality throughout our systems &amp; services.</li>
+             <li>Systematically evaluate and monitor the performance of our team</li>
+             <li>Provide timely delivery of services to our clients</li>
+             <li>Identify bottlenecks and subsequently eliminating them</li>
+             <li>Provide periodical training to our whole team so they understand the gravity of adhering to our Quality Management Protocol</li>
+           </ul>
+         </div>
+
+         <div class="container-fluid">
+           <h2>Quality Assurance</h2>
+           <p>Future Leap’s methods of Quality Assurance are very systematic in ensuring that a deliverable is not only acceptable in terms of international standards but resonates perfection of our services. Our Quality Assurance process is surely a reflection our overall project performance which mandates that the deliverables are tested thoroughly and are able to satisfy our clients’ demands among the highest attainable standards.</p>
+         </div>
        </div>
     </div>
 
