b473b3548c87f048c489137b4e5fab0d587c88b1
jackie247s <jackie247s@gmail.com>
2017-06-11 15:57:58 +0500

    Added content to quality policy page

--- quality-policy.php
+++ quality-policy.php
@@ -14,7 +14,18 @@
         require_once 'includes/header.php';
        ?>
 
-       
+       <div class="quality-policy col-md-8">
+         <h2>Quality Policy</h2>
+         <p>Our quality policy has been defined with the aim of enhancing our services to our customers in terms of better understanding of customer requirements, designing optimal solutions to meet those requirements and building systems and processes to ensure the highest standards of quality at all costs.We are committed to:</p>
+         <ul>
+           <li>Developing and improving Quality Management System periodically</li>
+           <li>Monitoring process towards building quality throughout our systems &amp; services.</li>
+           <li>Systematically evaluate and monitor the performance of our team</li>
+           <li>Provide timely delivery of services to our clients</li>
+           <li>Identify bottlenecks and subsequently eliminating them</li>
+           <li>Provide periodical training to our whole team so they understand the gravity of adhering to our Quality Management Protocol</li>
+         </ul>
+       </div>
     </div>
 
     <?php
