5d0de1ca6f0332b997e43b5090f809b892eb5f29
jackie247s <jackie247s@gmail.com>
2017-06-15 15:16:00 +0500

    Added MEP Page

---
+++ mep.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>FutureLeap</title>
+    <?php
+      require_once 'includes/head-links.php';
+     ?>
+     <link rel="stylesheet" href="css/jquery.bxslider.min.css">
+  </head>
+  <body>
+
+    <div class="page">
+      <?php
+        require_once 'includes/header.php';
+       ?>
+
+       <div class="mep">
+
+         <div class="slider">
+           <ul class="bxslider">
+             <li><img src="images/slider1.jpg" /></li>
+             <li><img src="images/slider2.jpg" /></li>
+             <li><img src="images/slider3.jpg" /></li>
+           </ul>
+         </div>
+
+         <div class="col-md-8 col-lg-8">
+           <h2>MEP</h2>
+           <p>Our dedicated team of engineers, under the supervision of highly qualified and experienced experts, covers the entire range of Mechanical, Electrical, and Plumbing works. In short, these include HVAC (Heating Ventilation & Air-Conditioning), electrical works, and plumbing and drainage. Future Leap’s team of workers, project managers, designers, engineers and supervisors are nothing less than perfectionists who obsess over executing projects with an extra-ordinary precision and skills.</p>
+
+           
+         </div>
+
+       </div>
+    </div>
+
+
+    <?php
+      require_once 'includes/scripts.php';
+      require_once 'includes/bxslider.php';
+     ?>
+  </body>
+</html>
