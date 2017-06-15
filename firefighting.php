8be55b86d82ec984cd2c4038f1e14a01e8228891
jackie247s <jackie247s@gmail.com>
2017-06-15 15:22:03 +0500

    Added Firefighting Page

---
+++ firefighting.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>FutureLeap</title>
+    <?php
+      require_once 'includes/head-links.php';
+     ?>
+     <link rel="stylesheet" href="css/jquery.bxslider.min.css">
+  </head>
+  <body>
+
+    <div class="page">
+      <?php
+        require_once 'includes/header.php';
+       ?>
+
+       <div class="firefighting">
+
+         <div class="slider">
+           <ul class="bxslider">
+             <li><img src="images/slider1.jpg" /></li>
+             <li><img src="images/slider2.jpg" /></li>
+             <li><img src="images/slider3.jpg" /></li>
+           </ul>
+         </div>
+
+         <div class="col-md-8 col-lg-8">
+           <h2>Scope Of Work</h2>
+           <ul>
+             <li>Integrated Fire Alarm System</li>
+             <li>Wet Riser</li>
+             <li>Dry Riser (Seamless / Welded / Galvanized Pipe works)</li>
+             <li>Threaded / Grooved Fittings</li>
+             <li>Pipe Supports</li>
+             <li>Fire Fighting Pumps</li>
+             <li>Hose Reel Cabinets</li>
+             <li>Fire Extinguishers</li>
+             <li>Hydraulically calculated Sprinkler System (Piping, Valves / Controls)</li>
+             <li>Alarm Check &amp; Landing Valves</li>
+             <li>Emergency lights &amp; Signage</li>
+             <li>Supervisory Devices &amp; Tamper Switch</li>
+             <li>Flow Switches</li>
+             <li>Complete System Accessories</li>
+             <li>Test / Drain Valves &amp; Pressure Gauges</li>
+             <li>Fire Hydrants &amp; Fire Department Connections</li>
+           </ul>
+
+         </div>
+
+       </div>
+    </div>
+
+
+    <?php
+      require_once 'includes/scripts.php';
+      require_once 'includes/bxslider.php';
+     ?>
+  </body>
+</html>
