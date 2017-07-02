abcdfb348b5c388b27108df29db0cf0867194a83
jackie247s <jackie247s@gmail.com>
2017-07-02 09:50:53 +0500

    Added local copies of jquery and bxslider, changed head-links.php and scripts.php to use local copies

--- includes/bxslider.php
+++ includes/bxslider.php
@@ -1,4 +1,5 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.12/jquery.bxslider.min.js"></script>
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.12/jquery.bxslider.min.js"></script> -->
+<script src="jquery.bxslider/jquery.bxslider.min.js" charset="utf-8"></script>
 <script type="text/javascript">
 $(document).ready(function(){
   $('.bxslider').bxSlider();
