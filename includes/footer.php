79bf872902d23a4932bc8e741a021bdeb02b4308
jackie247s <jackie247s@gmail.com>
2017-07-05 11:22:42 +0500

    Fixed link for quality policy,ehs policy

--- includes/footer.php
+++ includes/footer.php
@@ -4,7 +4,7 @@
     <p><a href="index.php"><img src="images/logo.jpg" id="footer-logo" alt=""></a> © 2017 FutureLeap Group. All Rights Reserved</p>
   </div>
   <div class="col-md-offset-2 col-md-4 bottom-right">
-    <a href="ehs-policy">EHS Policy</a>
-    <a href="quality-policy">Quality Policy</a>
+    <a href="ehs-policy.php">EHS Policy</a>
+    <a href="quality-policy.php">Quality Policy</a>
   </div>
 </footer>
