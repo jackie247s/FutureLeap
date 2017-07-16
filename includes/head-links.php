6150ecb0c607cb073df3770c2fbe8edd1eac93c1
jackie247s <jackie247s@gmail.com>
2017-07-16 14:03:59 +0500

    Added favicon image

--- includes/head-links.php
+++ includes/head-links.php
@@ -1,3 +1,4 @@
 <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 <link rel="stylesheet" href="css/style.css">
- <link href="https://fonts.googleapis.com/css?family=Abel" rel="stylesheet"> 
+ <link href="https://fonts.googleapis.com/css?family=Abel" rel="stylesheet">
+ <link rel="shortcut icon" type="image/ico" href="images/favicon.ico">
