85a6647d1aa276115c113340da709e48bdb34673
jackie247s <jackie247s@gmail.com>
2017-07-22 18:08:00 +0500

    Wrote PHP Form Handler for career data into career page, Inserted new html elements for showing success and failure

--- career.php
+++ career.php
@@ -1,3 +1,43 @@
+<?php
+
+  require_once 'scripts/vendor/PHPMailer/class.phpmailer.php';
+
+  if (isset($_POST) && isset($_FILES['cv'])) {
+    $target_dir = "uploads/";
+    $file = $_FILES['cv'];
+    $filename = $file['name'];
+    $filetempname = $file['tmp_name'];
+    $target_file = $target_dir.basename($filename);
+
+    $errors = array();
+
+
+    if (move_uploaded_file($filetempname, $target_file)) {
+      $email = new PHPMailer();
+
+      $email->Body = "CV sent by Potential Employee";
+      $email->Subject   = 'FutureLeap Career';
+      $email->AddAddress( 'jackie247s@gmail.com' );
+
+      $file_to_attach = 'PATH_OF_YOUR_FILE_HERE';
+
+      $email->AddAttachment( $target_file , $filename );
+
+      if ($email->send()) {
+        $success = true;
+      }
+      else{
+        echo "Mailer Error: " . $email->ErrorInfo;
+      }
+
+    }
+    else{
+      $failure = true;
+    }
+  }
+
+ ?>
+
 <!DOCTYPE html>
 <html>
   <head>
@@ -18,7 +58,24 @@
        <div class="career">
          <img id="career" src="images/career.jpg" alt="">
 
-           <form enctype="multipart/form-data" method="POST" class="career-form form-horizontal">
+          <?php
+            if (isset($success)) {
+              ?>
+              <div id='career-form-success'>
+                <p>Your CV has been successfully sent. Please wait for our response.</p>
+              </div>
+              <?php
+            }
+            elseif (isset($failure)) {
+              ?>
+              <div id="career-form-failure">
+                <p>An error occurred. The file could not be uploaded</p>
+              </div>
+              <?php
+            }
+           ?>
+
+           <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" enctype="multipart/form-data" method="POST" class="career-form form-horizontal">
              <div class="form-group">
                <label class="control-label col-md-5" for="email">Upload Your CV:</label>
                <div class="col-md-5">
@@ -47,10 +104,6 @@
       require_once 'includes/scripts.php';
      ?>
      <script type="text/javascript">
-
-      $(document).ready(function(){
-
-      });
 
      </script>
   </body>
